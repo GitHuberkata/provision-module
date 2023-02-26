3a40146b2d2bd1bdac298d7b5cc53ac30e72d3c6
GitHuberkata <petyagavrilova86@gmail.com>
2023-02-26 19:04:30 +0200

    Update provision.tf

--- provision.tf
+++ provision.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 module "networking" {
-    source = "https://github.com/GitHuberkata/network-module/releases/tag/v1.0.2"
+    source = "https://github.com/GitHuberkata/network-module/archive/refs/tags/v1.0.2.zip"
 }
 
 # Create aws server using previously created image/ami with installed nginx with Packer
